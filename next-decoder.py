3cf00f28cb9ff76c110b964f412e8f036100a0b2
Joakim Bech <joakim.bech@linaro.org>
2020-01-06 12:49:05 +0100

    Initial version
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

---
+++ next-decoder.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import sys
+
+def is_sync(d, sz):
+    if sz < 10:
+        return False
+
+    for i in range(0, 10):
+        if d[i] != "0":
+            return False
+    #print("Found sync msg")
+    return True
+
+def is_zero_bit(d, sz):
+    if sz < 5:
+        return False
+
+    for i in range(0, 5):
+        if d[i] != "0":
+            return False
+    #print("Found zero bit")
+    return True
+
+def is_one_bit(d, sz):
+    if sz <= 1:
+        if d != "0":
+            return False
+    #print("Found one bit")
+    return True
+
+def main(d):
+    #print("{}".format(d))
+    i = 0
+    r = ""
+
+    while i < len(d):
+        if d[i] == "1":
+            i += 1
+            if len(d[i:]) == 0:
+                break
+
+            if is_sync(d[i:], len(d[i:])):
+                r += "1"
+                i += 10
+            elif is_zero_bit(d[i:], len(d[i:])):
+                r += "0"
+                i += 5
+            elif is_one_bit(d[i], 1):
+                r += "1"
+                i += 1
+        else:
+            i += 1
+            #print("error")
+    print("r: {}".format(r))
+    return r
+
+if __name__ == "__main__":
+    main(sys.argv[1])
